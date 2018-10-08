7e75e53b61923b7f1f36bfa3dedfae006e3243de
Daniel Aguilar <dacmail@gmail.com>
2018-10-08 15:19:27 +0200

    página estática y politica de privacidad en formulario

--- wp-content/themes/bicisparalavida/templates/content-page.php
+++ wp-content/themes/bicisparalavida/templates/content-page.php
@@ -1,2 +1,4 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'ungrynerd'), 'after' => '</p></nav>']); ?>
+<div class="container page__content">
+  <?php the_content(); ?>
+  <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'ungrynerd'), 'after' => '</p></nav>']); ?>
+</div>
--- wp-content/themes/bicisparalavida/templates/donation-form.php
+++ wp-content/themes/bicisparalavida/templates/donation-form.php
@@ -2,6 +2,7 @@
   <p><input name="donor_email" type="email" placeholder="Correo electrónico" required></p>
   <p><input name="donor_name" type="text" placeholder="Nombre y apellidos" required></p>
   <p><input name="donor_cp" type="text" pattern="[0-9]{5}" placeholder="Código postal" required></p>
+  <p><input name="accept" id="accept" type="checkbox" required> <label for="accept">Acepto la forma en que se <a href="<?= get_theme_mod('ungrynerd_policy'); ?>">tratan mis datos</a></label></p>
   <p><input name="donate_form" type="submit" value="Donar" class="button"></p>
   <?php wp_nonce_field('new_donation', 'new_donation_nonce'); ?>
   <input name="action" value="new_donation" type="hidden">
