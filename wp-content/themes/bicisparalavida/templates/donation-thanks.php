939f4e33f6f505a6d99a8be1ef1d53f4f81b78bd
Daniel Aguilar <dacmail@gmail.com>
2019-02-20 17:53:08 +0100

    Aviso en PDF

--- wp-content/themes/bicisparalavida/templates/donation-thanks.php
+++ wp-content/themes/bicisparalavida/templates/donation-thanks.php
@@ -2,12 +2,16 @@
   <article class="donate-page donate-page--thanks">
     <div class="container">
       <h1>¡Gracias!</h1>
-      <p>
-        <span id="track-id" class="track-code">
-          <span class="indications"><?php the_custom_logo(); ?> Introduce esta hoja en el embalaje de tu bicicleta para llevar el seguimiento de la donación. <?= do_shortcode('[qrcodetag]' . get_the_title() . '[/qrcodetag]') ?></span>
-          <?php the_title(); ?>
-        </span>
-      </p>
+      <div id="track-id" class="track-code">
+        <div class="indications">
+          <?php the_custom_logo(); ?>
+          <p>Introduce esta hoja en el embalaje de tu bicicleta para llevar el seguimiento de la donación. </p>
+          <h3>IMPORTANTE!</h3>
+          <p>Para poder hacer un correcto seguimiento de tu bicicleta, el número de serie generado en este documento debe ir incorporado en la bicicleta.</p>
+          <?= do_shortcode('[qrcodetag]' . get_the_title() . '[/qrcodetag]') ?>
+        </div>
+        <?php the_title(); ?>
+      </div>
       <p>Con este nº de seguimiento puedes comprobar el estado de tu donación. <strong>Imprime tu código de seguimiento y entregalo junto con tu bicicleta.</strong>. Si quieres donar más de una bicicleta deberás generar <strong>un código de seguimiento por cada bicicleta que dones.</strong></p>
       <p><a id="print" href="#" class="button">Imprimir código</a></p>
     </div>
