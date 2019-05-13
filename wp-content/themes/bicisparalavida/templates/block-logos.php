5cd3213a83a115c380dbcaf509a5d3a72ab5e895
Daniel Aguilar <dacmail@gmail.com>
2019-05-13 16:35:37 +0200

    Bloque logos colaboradores

--- wp-content/themes/bicisparalavida/templates/block-logos.php
+++ wp-content/themes/bicisparalavida/templates/block-logos.php
@@ -6,3 +6,13 @@
     <?php endforeach; ?>
   </div>
 </section>
+
+<section class="logos logos--collab">
+  <h3 class="step__title"><?php esc_html_e('Colaboran:', 'ungrynerd'); ?></h3>
+  <div class="container">
+    <?php $logos = get_field('logos_collab', get_option('page_on_front')); ?>
+    <?php foreach ($logos as $logo) : ?>
+      <a target="_blank" class="logos__logo" href="<?= get_field('link_logo', $logo['ID']) ?>">  <?= wp_get_attachment_image($logo['ID'], 'un_medium', false, array('class' => 'logos__image'));?></a>
+    <?php endforeach; ?>
+  </div>
+</section>
