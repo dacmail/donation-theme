0c72c4e00a2a4950835dc462a848134d16c49f51
Daniel Aguilar <dacmail@gmail.com>
2019-05-13 16:40:58 +0200

    Comprobación que hay logos colaboradores

--- wp-content/themes/bicisparalavida/templates/block-logos.php
+++ wp-content/themes/bicisparalavida/templates/block-logos.php
@@ -1,18 +1,22 @@
-<section class="logos">
-  <div class="container">
-    <?php $logos = get_field('logos', get_option('page_on_front')); ?>
-    <?php foreach ($logos as $logo) : ?>
-      <a target="_blank" class="logos__logo" href="<?= get_field('link_logo', $logo['ID']) ?>">  <?= wp_get_attachment_image($logo['ID'], 'un_medium', false, array('class' => 'logos__image'));?></a>
-    <?php endforeach; ?>
-  </div>
-</section>
+<?php $logos = get_field('logos', get_option('page_on_front')); ?>
+<?php if (!empty($logos)) : ?>
+  <section class="logos">
+    <div class="container">
+      <?php foreach ($logos as $logo) : ?>
+        <a target="_blank" class="logos__logo" href="<?= get_field('link_logo', $logo['ID']) ?>">  <?= wp_get_attachment_image($logo['ID'], 'un_medium', false, array('class' => 'logos__image'));?></a>
+      <?php endforeach; ?>
+    </div>
+  </section>
+<?php endif; ?>
 
-<section class="logos logos--collab">
-  <h3 class="step__title"><?php esc_html_e('Colaboran:', 'ungrynerd'); ?></h3>
-  <div class="container">
-    <?php $logos = get_field('logos_collab', get_option('page_on_front')); ?>
-    <?php foreach ($logos as $logo) : ?>
-      <a target="_blank" class="logos__logo" href="<?= get_field('link_logo', $logo['ID']) ?>">  <?= wp_get_attachment_image($logo['ID'], 'un_medium', false, array('class' => 'logos__image'));?></a>
-    <?php endforeach; ?>
-  </div>
-</section>
+<?php $logos_collab = get_field('logos_collab', get_option('page_on_front')); ?>
+<?php if (!empty($logos_collab)) : ?>
+  <section class="logos logos--collab">
+    <h3 class="step__title"><?php esc_html_e('Colaboran:', 'ungrynerd'); ?></h3>
+    <div class="container">
+      <?php foreach ($logos_collab as $logo) : ?>
+        <a target="_blank" class="logos__logo" href="<?= get_field('link_logo', $logo['ID']) ?>">  <?= wp_get_attachment_image($logo['ID'], 'un_medium', false, array('class' => 'logos__image'));?></a>
+      <?php endforeach; ?>
+    </div>
+  </section>
+<?php endif; ?>
