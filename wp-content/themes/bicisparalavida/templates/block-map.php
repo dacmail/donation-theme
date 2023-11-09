9f97e2de52ff2841d2a24d1d56da163c256edbe3
Daniel Aguilar <dacmail@gmail.com>
2023-11-09 09:59:07 +0100

    new seur map

--- wp-content/themes/bicisparalavida/templates/block-map.php
+++ wp-content/themes/bicisparalavida/templates/block-map.php
@@ -1,6 +1,6 @@
 <section class="map">
   <h2 class="map__title">Localiza tu punto de entrega más cercano</h2>
   <div class="map__container">
-    <iframe src="https://www.google.com/maps/d/embed?mid=1QBs5zcamYdDgJnrtd_-jNKEax0zjktAv" width="640" height="480"></iframe>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1G9gsxCIVnxu7wxowFznZRkzrau_RtGw" width="640" height="480"></iframe>
   </div>
 </section>
